fd46d346d6028d722a32f59c96fc7066eea68702
sungju-kim <wnwn0719@gmail.com>
2022-10-27 18:06:40 +0900

    Feat: Custom Font 구현

--- BookSearcher/BookSearcher/Extension/UIFont + extension.swift
+++ BookSearcher/BookSearcher/Extension/UIFont + extension.swift
@@ -5,4 +5,29 @@
 //  Created by dale on 2022/10/27.
 //
 
-import Foundation
+import UIKit.UIFont
+
+extension UIFont {
+    class func customFont(ofSize: CGFloat, weight: CustomWeight) -> UIFont? {
+        return UIFont(name: weight.fontName, size: ofSize)
+    }
+
+    enum CustomWeight: String {
+        case medium = "Medium"
+        case regular = "Regular"
+        case semibold = "Semibold"
+        case bold = "Bold"
+
+        private var fontType: String {
+            return "SFProDisplay"
+        }
+
+        private var weight: String {
+            return rawValue
+        }
+
+        var fontName: String {
+            return "\(fontType)-\(weight)"
+        }
+    }
+}

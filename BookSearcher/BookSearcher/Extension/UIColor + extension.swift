3ef3d80bc486610bd7ac055ac25d392ded061c8d
sungju-kim <wnwn0719@gmail.com>
2022-10-27 23:37:57 +0900

    Add: selectedItem Color 추가
    #1

--- BookSearcher/BookSearcher/Extension/UIColor + extension.swift
+++ BookSearcher/BookSearcher/Extension/UIColor + extension.swift
@@ -11,5 +11,6 @@ extension UIColor {
     enum Custom {
         static let background = UIColor(red: 38/255, green: 37/255, blue: 42/255, alpha: 1)
         static let placeholder = UIColor(red: 165/255, green: 171/255, blue: 175/255, alpha: 1)
+        static let selectedItem = UIColor.systemCyan
     }
 }

878c120dfa83c7c961b864d4d4364673f11d6fa6
sungju-kim <wnwn0719@gmail.com>
2022-10-28 01:30:29 +0900

    Feat: headerLabel, moreButton 구현
    #1

--- BookSearcher/BookSearcher/Scene/Main/MainViewController.swift
+++ BookSearcher/BookSearcher/Scene/Main/MainViewController.swift
@@ -34,11 +34,34 @@ final class MainViewController: UIViewController {
 
     private let menuBar = MenuBar()
 
+    private let headerLabel: UILabel = {
+        let label = UILabel()
+        label.font = .customFont(ofSize: 28, weight: .regular)
+        label.textColor = .Custom.textWhite
+        return label
+    }()
+
+    private let moreButton: UIButton = {
+        var configuration = UIButton.Configuration.plain()
+        configuration.image = UIImage(systemName: "chevron.right")
+        configuration.baseForegroundColor = .Custom.selectedItem
+        let button = UIButton(configuration: configuration)
+        return button
+    }()
+
+    private lazy var headerContainer: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        [headerLabel, moreButton].forEach { stackView.addArrangedSubview($0) }
+        return stackView
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
 
         layoutSearchButton()
         layoutMenuBar()
+        layoutHeaderContainer()
     }
 }
 
@@ -60,6 +83,15 @@ private extension MainViewController {
             make.top.equalTo(searchButton.snp.bottom).offset(Constraint.regular)
             make.leading.trailing.equalTo(searchButton)
             make.height.equalTo(50)
+        }
+    }
+
+    func layoutHeaderContainer() {
+        view.addSubview(headerContainer)
+
+        headerContainer.snp.makeConstraints { make in
+            make.top.equalTo(menuBar.snp.bottom).offset(Constraint.regular)
+            make.leading.trailing.equalToSuperview().inset(Constraint.regular)
         }
     }
 }

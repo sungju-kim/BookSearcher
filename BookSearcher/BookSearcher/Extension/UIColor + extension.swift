04e124a26670cbbcb7dd9d8364c71f5c806e3794
sungju-kim <wnwn0719@gmail.com>
2022-10-27 19:09:01 +0900

    Feat: background color 변경 및 placeholder color 추가
    #1

--- BookSearcher/BookSearcher/Extension/UIColor + extension.swift
+++ BookSearcher/BookSearcher/Extension/UIColor + extension.swift
@@ -9,6 +9,7 @@ import UIKit.UIColor
 
 extension UIColor {
     enum Custom {
-        static let background = UIColor.systemBackground
+        static let background = UIColor(red: 38/255, green: 37/255, blue: 42/255, alpha: 1)
+        static let placeholder = UIColor(red: 165/255, green: 171/255, blue: 175/255, alpha: 1)
     }
 }

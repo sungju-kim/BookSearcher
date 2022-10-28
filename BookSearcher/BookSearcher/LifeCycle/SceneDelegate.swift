91ab8623d3b9c69bde39150d619a5acf57173665
sungju-kim <wnwn0719@gmail.com>
2022-10-28 22:00:53 +0900

    Feat: ViewModel -> MainViewController 주입

--- BookSearcher/BookSearcher/LifeCycle/SceneDelegate.swift
+++ BookSearcher/BookSearcher/LifeCycle/SceneDelegate.swift
@@ -13,10 +13,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = scene as? UIWindowScene else { return }
 
+        let viewController = MainViewController()
+        let viewModel = MainViewModel()
+        viewController.configure(with: viewModel)
+
         window = UIWindow(windowScene: windowScene)
         window?.overrideUserInterfaceStyle = .light
         window?.backgroundColor = .Custom.background
-        window?.rootViewController = MainViewController()
+        window?.rootViewController = viewController
         window?.makeKeyAndVisible()
     }
 }

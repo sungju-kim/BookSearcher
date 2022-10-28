f12c73abb9c87c04deee8e3d3b17ba2254019d30
sungju-kim <wnwn0719@gmail.com>
2022-10-28 12:18:39 +0900

    Add: NetworkError
    #9

---
+++ BookSearcher/BookSearcher/Network/NetworkError.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkError.swift
+//  BookSearcher
+//
+//  Created by dale on 2022/10/28.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case invalidURL
+    case failToDecode
+    case emptyData
+}

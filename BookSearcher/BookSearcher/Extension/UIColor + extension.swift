e6f1bf2dfca4ede5cb1d3282cb5cbe15204aea3a
sungju-kim <wnwn0719@gmail.com>
2022-10-28 17:06:32 +0900

    Add: UIColor.Custom.animateGray
    #5

--- BookSearcher/BookSearcher/Extension/UIColor + extension.swift
+++ BookSearcher/BookSearcher/Extension/UIColor + extension.swift
@@ -11,6 +11,7 @@ extension UIColor {
     enum Custom {
         static let background = UIColor(red: 38/255, green: 37/255, blue: 42/255, alpha: 1)
         static let placeholder = UIColor(red: 165/255, green: 171/255, blue: 175/255, alpha: 1)
+        static let animateGray = UIColor(red: 51/255, green: 50/255, blue: 57/255, alpha: 1)
         static let selectedItem = UIColor.systemCyan
         static let textGray = UIColor(red: 165/255, green: 171/255, blue: 175/255, alpha: 1)
         static let textWhite = UIColor(red: 238/255, green: 238/255, blue: 241/255, alpha: 1)

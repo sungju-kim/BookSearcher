9b0897f9d6dedd68730509baf5b94afb99efe6e8
sungju-kim <wnwn0719@gmail.com>
2022-10-30 19:50:49 +0900

    Add: SearchResultCell
    #2

---
+++ BookSearcher/BookSearcher/Scene/SearhResult/Cell/SearchResultCell.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResultCell.swift
+//  BookSearcher
+//
+//  Created by dale on 2022/10/30.
+//
+
+import UIKit
+
+final class SearchResultCell: UITableViewCell {
+    static var identifier: String {
+        return "\(self)"
+    }
+}

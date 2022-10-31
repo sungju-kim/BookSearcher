29a10c67fed905f50a642c8322e6c7ac0ad60d2d
sungju-kim <wnwn0719@gmail.com>
2022-10-31 14:58:29 +0900

    Add: DetailViewController
    #4

---
+++ BookSearcher/BookSearcher/Scene/Detail/DetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.swift
+//  BookSearcher
+//
+//  Created by dale on 2022/10/31.
+//
+
+import UIKit
+
+final class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

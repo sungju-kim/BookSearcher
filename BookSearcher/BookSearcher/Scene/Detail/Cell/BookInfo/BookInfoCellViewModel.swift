477afb5919fbf38dd4e5b95d3048d97e28fa37d0
sungju-kim <wnwn0719@gmail.com>
2022-11-01 20:03:55 +0900

    Feat: BookInfoCellViewModel 구현
    #16

--- BookSearcher/BookSearcher/Scene/Detail/Cell/BookInfo/BookInfoCellViewModel.swift
+++ BookSearcher/BookSearcher/Scene/Detail/Cell/BookInfo/BookInfoCellViewModel.swift
@@ -6,17 +6,27 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+import RxRelay
 
 final class BookInfoCellViewModel: CellViewModel {
+    private let disposeBag = DisposeBag()
     struct Input {
+        let cellDidLoad = PublishRelay<Void>()
     }
 
     struct Output {
+        let didLoadText = PublishRelay<String>()
     }
 
     let input = Input()
     let output = Output()
 
     init(text: String?) {
+        input.cellDidLoad
+            .compactMap { text }
+            .bind(to: output.didLoadText)
+            .disposed(by: disposeBag)
     }
 }

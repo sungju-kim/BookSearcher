0b8374409f6999267308502c3083128134e0e15f
sungju-kim <wnwn0719@gmail.com>
2022-11-02 01:16:04 +0900

    Feat: ReviewCell 구현
    #16

--- BookSearcher/BookSearcher/Scene/Detail/Cell/Review/ReviewCell.swift
+++ BookSearcher/BookSearcher/Scene/Detail/Cell/Review/ReviewCell.swift
@@ -6,10 +6,73 @@
 //
 
 import UIKit
+import SnapKit
 
 final class ReviewCell: UICollectionViewCell {
     static var identifier: String {
         return "\(self)"
+    }
+
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+    private let nameLabel: UILabel = {
+        let label = CustomLabel(fontColor: .white, fontSize: 14, fontWeight: .semibold)
+        return label
+    }()
+
+    private let starRateView = StarRateView()
+
+    private let dateLabel: UILabel = {
+        let label = CustomLabel(fontColor: .Custom.textGray, fontSize: 14, fontWeight: .regular)
+        return label
+    }()
+
+    private lazy var dateContainer: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = Constraint.min / 2
+
+        [starRateView, dateLabel].forEach { stackView.addArrangedSubview($0) }
+        return stackView
+    }()
+
+    private let reviewConent: UILabel = {
+        let label = CustomLabel(fontColor: .Custom.textGray, fontSize: 14, fontWeight: .regular)
+        label.numberOfLines = 3
+        return label
+    }()
+
+    private lazy var container: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = Constraint.min
+
+        [nameLabel, dateContainer, reviewConent].forEach { stackView.addArrangedSubview($0) }
+        return stackView
+    }()
+
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+
+        imageView.layer.cornerRadius = imageView.frame.height / 2
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        layoutImageView()
+        layoutContainer()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+
+        layoutImageView()
+        layoutContainer()
     }
 }
 
@@ -17,5 +80,27 @@ final class ReviewCell: UICollectionViewCell {
 extension ReviewCell {
     func configure(with viewModel: ReviewCellViewModel) {
 
+    }
+}
+
+// MARK: - Layout Section
+
+private extension ReviewCell {
+    func layoutImageView() {
+        contentView.addSubview(imageView)
+
+        imageView.snp.makeConstraints { make in
+            make.width.height.equalTo(50)
+            make.top.leading.equalToSuperview()
+        }
+    }
+
+    func layoutContainer() {
+        contentView.addSubview(container)
+
+        container.snp.makeConstraints { make in
+            make.leading.equalTo(imageView.snp.trailing).offset(Constraint.regular)
+            make.top.bottom.trailing.equalToSuperview()
+        }
     }
 }

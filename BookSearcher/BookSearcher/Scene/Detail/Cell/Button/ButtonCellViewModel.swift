f886039f9428613ce2efddca9c908a7ee38f84e7
sungju-kim <wnwn0719@gmail.com>
2022-11-01 19:59:25 +0900

    Feat: ButtonCellViewModel 구현
    #16

--- BookSearcher/BookSearcher/Scene/Detail/Cell/Button/ButtonCellViewModel.swift
+++ BookSearcher/BookSearcher/Scene/Detail/Cell/Button/ButtonCellViewModel.swift
@@ -6,17 +6,28 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+import RxRelay
 
 final class ButtonCellViewModel: CellViewModel {
+    private let disposeBag = DisposeBag()
     struct Input {
+        let linkButtonTapped = PublishRelay<Void>()
     }
 
     struct Output {
+        let prepareForPresent = PublishRelay<URL>()
     }
 
     let input = Input()
     let output = Output()
 
     init(link: String?) {
+        input.linkButtonTapped
+            .compactMap { link }
+            .compactMap { URL(string: $0) }
+            .bind(to: output.prepareForPresent)
+            .disposed(by: disposeBag)
     }
 }

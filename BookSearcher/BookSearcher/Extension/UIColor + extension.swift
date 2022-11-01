dc0391ff56edc369417a766e7276955867bc8c77
sungju-kim <wnwn0719@gmail.com>
2022-11-02 00:41:29 +0900

    Add: UIColor.Custom 추가
    #16

--- BookSearcher/BookSearcher/Extension/UIColor + extension.swift
+++ BookSearcher/BookSearcher/Extension/UIColor + extension.swift
@@ -16,5 +16,7 @@ extension UIColor {
         static let textGray = UIColor(red: 165/255, green: 171/255, blue: 175/255, alpha: 1)
         static let textWhite = UIColor(red: 238/255, green: 238/255, blue: 241/255, alpha: 1)
         static let buttonBlue = UIColor(red: 92/255, green: 189/255, blue: 246/255, alpha: 1)
+        static let starRate = UIColor(red: 5/255, green: 163/255, blue: 249/255, alpha: 1)
+        static let rateBackground = UIColor(red: 69/255, green: 73/255, blue: 80/255, alpha: 1)
     }
 }

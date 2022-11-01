355b2ee169c13b37629dac25edbd46cbb05f102d
sungju-kim <wnwn0719@gmail.com>
2022-11-01 23:45:30 +0900

    Feat: StarRateCell 구현
    #16

--- BookSearcher/BookSearcher/Scene/Detail/Cell/StarRate/StarRateCell.swift
+++ BookSearcher/BookSearcher/Scene/Detail/Cell/StarRate/StarRateCell.swift
@@ -6,16 +6,96 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
 
 final class StarRateCell: UICollectionViewCell {
+    private var disposeBag = DisposeBag()
+
+    private var viewModel: StarRateCellViewModel?
+
     static var identifier: String {
         return "\(self)"
+    }
+
+    private let ratingLabel: UILabel = {
+        let label = CustomLabel(fontColor: .white, fontSize: 60, fontWeight: .bold)
+        label.textAlignment = .center
+        return label
+    }()
+
+    private let starRateView = StarRateView()
+
+    private let countLabel: UILabel = {
+        let label = CustomLabel(fontColor: .Custom.textGray, fontSize: 12, fontWeight: .regular)
+        label.textAlignment = .center
+        return label
+    }()
+
+    private lazy var leftContainer: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = Constraint.min
+        stackView.distribution = .fill
+
+        [ratingLabel, starRateView, countLabel].forEach { stackView.addArrangedSubview($0) }
+        return stackView
+    }()
+
+    private let rateCountView: RateCountView = .init()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        layoutLeftContainer()
+        layoutRateCountView()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+
+        layoutLeftContainer()
+        layoutRateCountView()
     }
 }
 
 // MARK: - Configure
 extension StarRateCell {
     func configure(with viewModel: StarRateCellViewModel) {
+        self.viewModel = viewModel
 
+        viewModel.output.didLoadRate
+            .bind(to: ratingLabel.rx.text)
+            .disposed(by: disposeBag)
+
+        viewModel.output.didLoadStarRate
+            .bind(onNext: starRateView.configure)
+            .disposed(by: disposeBag)
+
+        viewModel.input.cellDidLoad.accept(())
+    }
+}
+
+// MARK: - Layout Section
+
+private extension StarRateCell {
+    func layoutLeftContainer() {
+        contentView.addSubview(leftContainer)
+
+        leftContainer.snp.makeConstraints { make in
+            make.leading.top.bottom.equalToSuperview()
+            make.width.equalTo(ratingLabel.intrinsicContentSize.width)
+        }
+    }
+
+    func layoutRateCountView() {
+        contentView.addSubview(rateCountView)
+
+        rateCountView.snp.makeConstraints { make in
+            make.leading.equalTo(leftContainer.snp.trailing).offset(Constraint.regular)
+            make.trailing.equalToSuperview()
+            make.top.equalTo(leftContainer)
+        }
     }
 }
